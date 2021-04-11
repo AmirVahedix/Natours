90c3341ce5f04c19e043ff7c51ed052c6d1af5e7
Amir Vahedi <amirvahedix@gmail.com>
2021-04-11 13:14:48 +0430

    Write media queries for Base, Typography and layout

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const menuCheckbox = document.querySelector('#navi-toggle');
 const menuList = document.querySelector('.navigation__list');
+menuList.style.visibility = 'hidden';
 
 menuCheckbox.addEventListener('click', (e) => {
     if (!e.target.checked) {
         menuList.style.visibility = 'hidden';
+        setTimeout(() => {
+            menuList.style.display = 'none';
+        }, 150);
     } else {
         menuList.style.visibility = 'visible';
+        menuList.style.display = 'inline-block';
     }
 })
